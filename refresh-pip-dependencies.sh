7998125b7fa1d66346a6f87ab5953a093bb65cb0
sir-maniac <608494+sir-maniac@users.noreply.github.com>
2025-10-17 20:45:24 -0600

    Update to version 49 of `org.gnome.Platform` runtime
    - update and remove dependencies
    - set `refresh-pip-dependencies` to ignore `packaging` and `PyGObject`,
    which are part of the runtime

--- refresh-pip-dependencies.sh
+++ refresh-pip-dependencies.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-./flatpak-builder-tools/pip/flatpak-pip-generator --requirements-file=../requirements.txt --output pypi-dependencies
+./flatpak-builder-tools/pip/flatpak-pip-generator \
+    --ignore-pkg 'packaging==24.1' 'PyGObject==3.52.3' \
+    --requirements-file=../requirements.txt \
+    --output pypi-dependencies
